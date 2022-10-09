cba43c6174a636ef066b1d72c856c40a7b997124
Mirko <mirko.poma00@gmail.com>
2022-10-09 18:53:29 +0200

    terzo commit, activity registrazione creata

--- app/src/main/java/com/example/an0nym0us/LoginActivity.kt
+++ app/src/main/java/com/example/an0nym0us/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.example.an0nym0us
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val registrati : TextView = findViewById(R.id.loginActivity_registrati)
+
+        registrati.setOnClickListener(){
+            val intent = Intent(this,RegisterActivity::class.java);
+            startActivity(intent);
+        }
     }
 }
