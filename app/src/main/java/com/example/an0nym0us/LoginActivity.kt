86e8258ca27c42eeacc6515ee4f628f8133e59b3
SimoneBernaschina <simone.berna01@gmail.com>
2022-10-10 18:51:41 +0200

    Quinto commit, implementazione firebase register/login

--- app/src/main/java/com/example/an0nym0us/LoginActivity.kt
+++ app/src/main/java/com/example/an0nym0us/LoginActivity.kt
@@ -3,7 +3,12 @@ package com.example.an0nym0us
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
@@ -12,12 +17,63 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         val registrati : TextView = findViewById(R.id.loginActivity_registrati)
+        val btn_login : Button = findViewById(R.id.loginActivity_Login)
+        val email : EditText = findViewById(R.id.loginActivity_email)
+        val password : EditText = findViewById(R.id.loginActivity_password)
 
         registrati.setOnClickListener(){
             val intent = Intent(this,RegisterActivity::class.java);
             startActivity(intent);
             overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left)
 
+        }
+
+        btn_login.setOnClickListener {
+            when{
+                TextUtils.isEmpty(email.text.toString().trim{it <= ' '}) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Inserisci email.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                TextUtils.isEmpty(password.text.toString().trim{it <= ' '}) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Inserisci password.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                else ->{
+                    val emailLogin : String = email.text.toString().trim{it <= ' '}
+                    val pwLogin : String = password.text.toString().trim{it <= ' '}
+
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(emailLogin, pwLogin).addOnCompleteListener { task ->
+                        if(task.isSuccessful){
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "Sei loggato correttamente",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            val intent = Intent(this@LoginActivity, HomepageActivity::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            intent.putExtra("user_id", FirebaseAuth.getInstance().currentUser!!.uid)
+                            intent.putExtra("email_id", emailLogin)
+                            startActivity(intent)
+                            finish()
+                        } else{
+                            Toast.makeText(
+                                this@LoginActivity,
+                                task.exception!!.message.toString(),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/an0nym0us/RegisterActivity.kt
+++ app/src/main/java/com/example/an0nym0us/RegisterActivity.kt
@@ -1,17 +1,79 @@
 package com.example.an0nym0us
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        val btn_register : Button = findViewById(R.id.registerActivity_Registrati)
+        val email : EditText = findViewById(R.id.registerActivity_email)
+        val password : EditText = findViewById(R.id.registerActivity_password)
+        val confirm_pw : EditText = findViewById(R.id.registerActivity_passwordRipeti)
+        btn_register.setOnClickListener{
+            when{
+                TextUtils.isEmpty(email.text.toString().trim{it <= ' '}) -> {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Inserisci email.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
 
+                TextUtils.isEmpty(password.text.toString().trim{it <= ' '}) -> {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Inserisci password.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
 
+                else -> {
+                    val emailInserita : String = email.text.toString().trim{it <= ' '}
+                    val passwordInserita : String = password.text.toString().trim{it <= ' '}
+
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailInserita, passwordInserita).addOnCompleteListener(
+                        OnCompleteListener<AuthResult>{ task ->
+                            if(task.isSuccessful) {
+                                val firebaseUser: FirebaseUser = task.result!!.user!!
+                                Toast.makeText(
+                                    this@RegisterActivity,
+                                    "Registrazione avvenuta con successo.",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                                val intent =
+                                    Intent(this@RegisterActivity, LoginActivity::class.java)
+                                intent.flags =
+                                    Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                intent.putExtra("user_id", firebaseUser.uid)
+                                intent.putExtra("email_id", emailInserita)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                Toast.makeText(
+                                    this@RegisterActivity,
+                                    task.exception!!.message.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                    )
+                }
+            }
+        }
     }
-
     override fun finish() {
         super.finish()
         overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right)
