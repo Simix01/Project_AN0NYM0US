4116d3e34c2f6d51488966f87f4561d1d3bcb4f1
Joshua Perez <joshuarandez06@gmail.com>
2022-11-01 19:00:01 +0100

    21 commit firebase download post to recycleView

--- app/src/main/java/com/example/an0nym0us/HomepageActivity.kt
+++ app/src/main/java/com/example/an0nym0us/HomepageActivity.kt
@@ -1,20 +1,15 @@
 package com.example.an0nym0us
 
 import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.target.SimpleTarget
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_homepage.*
-import kotlinx.android.synthetic.main.layout_post_list_item.*
 import kotlin.math.absoluteValue
 
 
@@ -23,39 +18,72 @@ class HomepageActivity : AppCompatActivity() {
     val valoreHash = cUser.hashCode().absoluteValue
     val uId = "anonym$valoreHash"
     private lateinit var postAdapter: PostRecyclerAdapter
+    private lateinit var list: ArrayList<Post2>
+    private lateinit var postRecyclerView:RecyclerView
+    private lateinit var dbRef:DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
         inizializzaBottomMenu()
         initRecyclerView()
-        addDataSet()
-        imageViewtoFragment()
-    }
 
-    private fun imageViewtoFragment() {
-
-        val mFragmentManager = supportFragmentManager
-        val mFragmentTransaction = mFragmentManager.beginTransaction()
-        val mFragment = PostFragment()
-
-        postAdapter.onImageClick={
-            recycler_view.visibility= View.INVISIBLE
-            val mBundle = Bundle()
-            mBundle.putParcelable("post",it)
-            mFragment.arguments = mBundle
-            mFragmentTransaction.add(R.id.fragment_container, mFragment).commit()
-        }
-    }
-
-    private fun addDataSet() {
-        val data = DataSource.createDataSet()
-        postAdapter.submitList(data)
     }
 
     private fun initRecyclerView() {
-        recycler_view.layoutManager = LinearLayoutManager(this)
-        postAdapter = PostRecyclerAdapter()
-        recycler_view.adapter = postAdapter
+        postRecyclerView=findViewById(R.id.recycler_view)
+        postRecyclerView.layoutManager = LinearLayoutManager(this)
+        postRecyclerView.setHasFixedSize(true)
+        list= arrayListOf<Post2>()
+        getPostData()
+    }
+
+    private fun getPostData() {
+        dbRef=FirebaseDatabase.getInstance("https://an0nym0usapp-default-rtdb.europe-west1.firebasedatabase.app/")
+            .getReference("Utenti")
+
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()) {
+                    for(userSnapshot in snapshot.children) {
+                        for(postSnapshot in userSnapshot.children) {
+                            var postApp= postSnapshot.getValue() as HashMap<*,*>
+                            var user = postApp["user"].toString()
+                            var category = postApp["category"].toString()
+                            var date = postApp["date"].toString()
+                            var image = postApp["image"].toString()
+                            var likes = postApp["likes"].toString()
+                            var dislikes = postApp["dislikes"].toString()
+                            var post =  Post2(date,image, dislikes.toInt(),category,user,likes.toInt())
+                            list.add(post)
+                        }
+                    }
+                    postAdapter = PostRecyclerAdapter(list)
+
+                    val mFragmentManager = supportFragmentManager
+                    val mFragmentTransaction = mFragmentManager.beginTransaction()
+                    val mFragment = PostFragment()
+
+                    postAdapter.onImageClick={
+                        recycler_view.visibility= View.INVISIBLE
+                        val mBundle = Bundle()
+
+                        val post = Post(it.user!!,it.category!!,it.date!!,it.image!!,it.likes,it.dislikes)
+
+                        mBundle.putParcelable("post",post)
+                        mFragment.arguments = mBundle
+                        mFragmentTransaction.add(R.id.fragment_container, mFragment).commit()
+                    }
+
+                    postRecyclerView.adapter = postAdapter
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 
     private fun inizializzaBottomMenu() {
